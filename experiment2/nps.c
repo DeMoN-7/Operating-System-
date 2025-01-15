bc480a556294bd0c773635b04b38596d57119b9f
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-01-15 11:20:14 +0530

    commit

--- experiment2/nps.c
+++ experiment2/nps.c
@@ -1,11 +1,17 @@
 // Turnaround Time=Completion Time−Arrival Time
 // Waiting Time=Turnaround Time−Burst Time
 // Response Time=Start Time−Arrival Time
-
-
-
+#define MAX 100
+#include<string.h>
 #include<stdio.h>
 void main(){
-    int n=4;
-    int pid[]={0,1,7,3};
+    FILE *file=fopen("input.txt");
+    int pid[MAX], arrival_time[MAX], burst_time[MAX], priority[MAX];
+    fscanf(file, "%d", &n);  
+    for (int i = 0; i < n; i++) {
+        fscanf(file, "%d %d %d %d", &pid[i], &arrival_time[i], &burst_time[i], &priority[i]);
+    }
+    fclose(file);
+        printf("%d %d %d %d", pid[i], arrival_time[i], burst_time[i], priority[i]);
+    
 }
